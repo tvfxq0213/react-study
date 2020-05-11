15a7ae90bc2140d49c219cce5df16c35a51f8b18
narae <alas0213@naver.com>
2020-05-11 22:33:51 +0900

    3-3 반목문까지 수강완료

---
+++ 3.숫자야구/NumberBaseball.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+const NumberBaseball = () => {
+  const [result, setResult] = useState();
+  const [answer, setAnswer] = useState('제로초');
+  const [value, setValue] = useState('');
+  const [tries, setTries] = useState('');
+}
