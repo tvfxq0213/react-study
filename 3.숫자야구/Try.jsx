937fe10ccb6c0e4ead71222a8576a055b377651b
narae <alas0213@naver.com>
2020-05-13 22:23:57 +0900

    3-9

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -10,7 +10,15 @@ class Try extends Component {
     )
   }
 }
-
-
+/*
+const Try = ({tryInfo}) => {
+  return (
+    <li >
+      <div>{tryInfo.try}</div>
+      <div>{tryInfo.result}</div>
+    </li>
+  )
+}
+*/
 
 export default Try;
