7809dc5f86f2f70f031c5d17ad6f7e81356680ac
narae <alas0213@naver.com>
2020-05-21 22:43:16 +0900

    6-3

---
+++ 5.가위바위보/RSP.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
+
+const rspCoords = {
+  바위: '0',
+  가위: '-142px',
+  보: '-284px',
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function(v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+//                        result, imgCoord, score
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
+
+// componentDidMount() {
+//   this.setState({
+//     imgCoord: 3,
+//     score: 1,
+//     result: 2,
+//   })
+// }
+
+// useEffect(() => {
+//   setImgCoord();
+//   setScore();
+// }, [imgCoord, score]);
+// useEffect(() => {
+//   setResult();
+// }, [result]);
+
+
+const RSP = () => {
+  const [result, setResult] = useState('');
+  const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+  const [score, setScore] = useState(0);
+  const interval = useRef();
+
+  useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
+    console.log('다시 실행');
+    interval.current = setInterval(changeHand, 100);
+    return () => { // componentWillUnmount 역할
+      console.log('종료');
+      clearInterval(interval.current);
+    }
+  }, [imgCoord]);
+//배열에는 꼭 useEffect를 다시 실행할 값만 넣어야 함
+
+  //useLayoutEffect(); 
+  //화면 리사이징을 감지할 때 사용됨, 화면 바뀌기 전에 발생함
+  const changeHand = () => {
+    if (imgCoord === rspCoords.바위) {
+      setImgCoord(rspCoords.가위);
+    } else if (imgCoord === rspCoords.가위) {
+      setImgCoord(rspCoords.보);
+    } else if (imgCoord === rspCoords.보) {
+      setImgCoord(rspCoords.바위);
+    }
+  };
+
+  const onClickBtn = (choice) => () => {
+    clearInterval(interval.current);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
+      setResult('비겼습니다!');
+    } else if ([-1, 2].includes(diff)) {
+      setResult('이겼습니다!');
+      setScore((prevScore) => prevScore + 1);
+    } else {
+      setResult('졌습니다!');
+      setScore((prevScore) => prevScore - 1);
+    }
+    setTimeout(() => {
+      interval.current = setInterval(changeHand, 100);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+      <div>
+        <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
+        <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
+        <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score}점</div>
+    </>
+  );
+};
+export default RSP;
