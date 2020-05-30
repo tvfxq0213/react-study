8b4499a6e306caf91adff78cbe6349b16d958cdc
narae <alas0213@naver.com>
2020-05-30 11:17:08 +0900

    완강

--- 8.지뢰찾기/Table.jsx
+++ 8.지뢰찾기/Table.jsx
@@ -2,7 +2,7 @@ import React, { useContext, memo } from 'react';
 import Tr from './Tr';
 import { TableContext } from './MineSearch';
 
-const Table = memo(() => {
+const Table = memo(() => { // memo적용하려면 하위컴포넌트에서 memo import 되어 있어야 함 
   const { tableData } = useContext(TableContext);
   return (
     <table>
