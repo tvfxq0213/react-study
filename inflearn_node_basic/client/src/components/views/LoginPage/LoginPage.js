9c92993990c0937cc014f7cf105710ec2b2fdde2
Narae <alas0213@naver.com>
2020-06-10 16:43:08 +0900

    push() 수정함

--- inflearn_node_basic/client/src/components/views/LoginPage/LoginPage.js
+++ inflearn_node_basic/client/src/components/views/LoginPage/LoginPage.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import {Router, Route, hashHistory} from 'react-router'; 
 import {useDispatch} from 'react-redux';
 import {loginUser} from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
 function LoginPage(props) {
 
@@ -63,4 +64,4 @@ function LoginPage(props) {
   )
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
--- inflearn_node_basic/client/src/components/views/RegisterPage/RegisterPage.js
+++ inflearn_node_basic/client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {useDispatch} from 'react-redux';
-import { withRouter } from 'react-router'; 
+import { withRouter } from 'react-router-dom';
 import {registerUser} from '../../../_actions/user_action';
 
 function RegisterPage(props) {
@@ -78,4 +78,4 @@ function RegisterPage(props) {
   
 }
 
-export default RegisterPage;
+export default withRouter(RegisterPage);
